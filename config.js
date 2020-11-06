ec04f09181191b62f92e9d3e3f3a2c0080ab3444
Buchuu <danielburru@gmail.com>
2020-11-06 01:59:34 +0100

    config
    c

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
   port: process.env.PORT || 3001,
-  db: process.env.MONGODB || 'mongodb://localhost:27017/buchspro',
+  db: process.env.MONGODB || 'mongodb://172.16.100.43:27017/buchspro',
   SECRET_TOKEN: '99f24e4249f28e71572ec40101db4fac'  //EbRu2c8h-s0p6r5o2 EN MD5
 }
